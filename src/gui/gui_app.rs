c14fe6d9d57e1f0581623f4d6314397184c30a9c
DerpyBurgerz <samuel@timetoknow.nl>
2026-06-12 21:42:23 +0200

    added random buttons

--- src/gui/gui_app.rs
+++ src/gui/gui_app.rs
@@ -1,6 +1,5 @@
-use std::time::Duration;
 use eframe::{Frame, Storage};
-use egui::{Context, RawInput, Ui, Visuals};
+use egui::{Context, RawInput, Ui};
 
 pub struct GuiApp{
 
@@ -8,44 +7,41 @@ pub struct GuiApp{
 
 impl GuiApp {
     pub fn new() -> GuiApp {
-        todo!()
+        Self{}
     }
 }
 
 impl eframe::App for GuiApp{
     fn logic(&mut self, ctx: &Context, frame: &mut Frame) {
-        todo!()
     }
 
     fn ui(&mut self, ui: &mut Ui, frame: &mut Frame) {
-        todo!()
+        egui::Panel::top("top_panel").show_inside(ui, |ui| {
+            // The top panel is often a good place for a menu bar:
+
+            egui::MenuBar::new().ui(ui, |ui| {
+                ui.menu_button("File", |ui| {
+                    if ui.button("Quit").clicked() {
+                        ui.send_viewport_cmd(egui::ViewportCommand::Close);
+                    }
+                });
+                ui.add_space(16.0);
+            });
+        });
+
+        egui::Panel::left("my_left_panel").show_inside(ui, |ui| {
+            ui.label("Hello World!");
+        });
     }
 
     fn update(&mut self, ctx: &Context, frame: &mut Frame) {
-        todo!()
     }
 
     fn save(&mut self, _storage: &mut dyn Storage) {
-        todo!()
     }
 
     fn on_exit(&mut self) {
-        todo!()
     }
-
-    fn auto_save_interval(&self) -> Duration {
-        todo!()
-    }
-
-    fn clear_color(&self, _visuals: &Visuals) -> [f32; 4] {
-        todo!()
-    }
-
-    fn persist_egui_memory(&self) -> bool {
-        todo!()
-    }
-
     fn raw_input_hook(&mut self, _ctx: &Context, _raw_input: &mut RawInput) {
-        todo!()
     }
 }
